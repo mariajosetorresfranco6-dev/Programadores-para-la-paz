b2c528ed7e8f3bb9af5984b042bf00945f62dd09
María José torres francos <mariajosetorresfranco6@gmail.com>
2026-06-11 19:42:07 -0500

    Initialize Express server with reportes endpoint

---
+++ Semana7/Server.js
@@ -0,0 +1,14 @@
+orma más rápida y eficiente.
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let reportes = [];
+
+app.get('/reportes', (req, res) => {
+  res.json(reportes);
+});
+
+app.post('/reportes', (req, res) => {
+  
