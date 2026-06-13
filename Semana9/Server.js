6f285c16b5b70546fe3a2fab499585f958f3adc5
María José torres francos <mariajosetorresfranco6@gmail.com>
2026-06-12 19:19:12 -0500

    Add basic server setup with status endpoint

---
+++ Semana9/Server.js
@@ -0,0 +1,13 @@
+funcionando');
+});
+
+app.get('/estado', (req, res) => {
+  res.json({
+    estado: "Servidor funcionando",
+    servicio: "API comunitaria"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
